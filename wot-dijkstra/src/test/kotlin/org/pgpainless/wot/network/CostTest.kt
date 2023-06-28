1005be095f622ed74421018b4cd989d8f1e5d402
Heiko Schaefer <heiko@schaefer.name>
2023-06-28 16:19:24 +0200

    Add test for Cost

---
+++ wot-dijkstra/src/test/kotlin/org/pgpainless/wot/network/CostTest.kt
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2023 Heiko Schaefer <heiko@schaefer@name>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.pgpainless.wot.dijkstra
+
+import kotlin.test.Test
+
+class CostTest {
+
+    @Test
+    fun cost() {
+        val cost1 = Cost(1, 60)
+        val cost2 = Cost(1, 120)
+
+        val cost3 = Cost(2, 60)
+        val cost4 = Cost(2, 120)
+
+        assert(cost1 < cost2)
+        assert(cost1 > cost3)
+
+        assert(cost2 > cost3)
+        assert(cost3 < cost4)
+
+        assert(cost1 > cost4)
+        assert(cost2 > cost4)
+    }
+
+}
